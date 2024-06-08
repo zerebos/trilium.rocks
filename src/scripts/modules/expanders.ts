a7ed566645862ece56b58915edcb38813ea40d5c
Zerebos <me@zerebos.com>
2024-06-08 14:31:40 -0400

    Improve page performance and rendering
    - Moves expanding the tree navigation to the template so the rendering is already done before page load
    - Adds a way to explicitly set the logo size to prevent the page moving after it loads in

--- src/scripts/modules/expanders.ts
+++ src/scripts/modules/expanders.ts
@@ -1,15 +1,15 @@
 // In case a linked article lead to a new tree
-const activeLink = document.querySelector("#menu a.active");
-if (activeLink) {
-    let parent = activeLink.parentElement;
-    const mainMenu = document.getElementById("#menu");
-    while (parent && parent !== mainMenu) {
-        if (parent.matches(".submenu-item") && !parent.classList.contains("expanded")) {
-            parent.classList.add("expanded");
-        }
-        parent = parent.parentElement;
-    }
-}
+// const activeLink = document.querySelector("#menu a.active");
+// if (activeLink) {
+//     let parent = activeLink.parentElement;
+//     const mainMenu = document.getElementById("#menu");
+//     while (parent && parent !== mainMenu) {
+//         if (parent.matches(".submenu-item") && !parent.classList.contains("expanded")) {
+//             parent.classList.add("expanded");
+//         }
+//         parent = parent.parentElement;
+//     }
+// }
 
 export default function setupExpanders() {
     const expanders = Array.from(document.querySelectorAll("#menu .submenu-item"));
